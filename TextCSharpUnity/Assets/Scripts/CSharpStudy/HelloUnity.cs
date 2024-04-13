3759f783cfdf32016941f41a0207f6b352c1f82f
jinjae84 <166198670+jinjae84@users.noreply.github.com>
2024-04-13 22:09:29 +0900

    (2024.04.06)Unity의 개념

---
+++ TextCSharpUnity/Assets/Scripts/CSharpStudy/HelloUnity.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HelloUnity : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, Unity!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
